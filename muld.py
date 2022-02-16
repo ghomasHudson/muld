027891d8ac502d169ffc8311d56fcaf0ae24d114
Thomas Hudson <07thomas.hudson@gmail.com>
2022-02-16 16:50:47 +0000

    Fix links

--- muld.py
+++ muld.py
@@ -1,4 +1,4 @@
-"""MuLD: The Multitask Long Document Benchmark"""
+"""The MuLD benchmark."""
 
 import json
 import os
@@ -38,8 +38,8 @@ The NarrativeQA Reading Comprehension Challenge Dataset consists of user-submitt
   publisher={MIT Press}
 }""",
         "urls": {
-            datasets.Split.TRAIN: "https://drive.google.com/uc?export=download&id=1sUXIC6lmk9Khp2mnr9VZwQ-StDlHqTw1?confirm=t",
-            datasets.Split.VALIDATION: "https://drive.google.com/uc?export=download&id=1xdXEhLHtcqOZh0FbPhY_dnvNMg2bALtm",
+            datasets.Split.TRAIN: "https://drive.google.com/uc?export=download&confirm=yTib&id=1sUXIC6lmk9Khp2mnr9VZwQ-StDlHqTw1",
+            datasets.Split.VALIDATION: "https://drive.google.com/uc?export=downloadid=1xdXEhLHtcqOZh0FbPhY_dnvNMg2bALtm",
             datasets.Split.TEST: "https://drive.google.com/uc?export=download&id=1BPBXyfYWVGtOXVQv_hlqtvbT25rTQzGu",
         }
 },
@@ -48,8 +48,8 @@ The NarrativeQA Reading Comprehension Challenge Dataset consists of user-submitt
         "description": """\
 The HotpotQA dataset consists of questions from crowd workers which require information from multiple Wikipedia articles in order to answer, thus testing the ability for models to perform multi-hop question answering. The data is commonly presented as a list of paragraphs containing relevant information plus a setting where the addition of 'distractor paragraphs' fully test the ability of the model to comprehend which information is relevant to the question asked. To transform this into a long document, we expand each paragraph with its full Wikipedia page as well as adding additional distractor articles from similar topics (randomly chosen from links on the existing pages) in order to meet the 10,000 token minimum length requirement for this benchmark. These articles are shuffled and concatenated to form the model input.""",
         "urls": {
-            datasets.Split.TRAIN: "https://drive.google.com/uc?export=download&id=1OlGRyCEL9JhwIQIKViaWIXCOB_pwj8xU&confirm=t",
-            datasets.Split.VALIDATION: "https://drive.google.com/uc?export=download&id=1_Svtg6PycBpezDYJ78zcJqLa8Ohnk6Gq&confirm=t"
+            datasets.Split.TRAIN: "https://drive.google.com/uc?export=download&confirm=yTib&id=1OlGRyCEL9JhwIQIKViaWIXCOB_pwj8xU",
+            datasets.Split.VALIDATION: "https://drive.google.com/uc?export=download&confirm=yTib&id=1_Svtg6PycBpezDYJ78zcJqLa8Ohnk6Gq"
         }
 },
 
